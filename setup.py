5c4ce5e7476b6f35c2917efa835c4f1a0f308ebf
Zhongpu Chen <chenloveit@gmail.com>
2019-05-06 16:29:49 +0800

    update to 0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="osmRoad",
-    version="0.1",
+    version="0.2",
     author="Zhongpu Chen",
     author_email="chenloveit@gmail.com",
     description="Parse OSM Data",
